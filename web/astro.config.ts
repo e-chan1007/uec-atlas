0192134cfa2a84e31c4c06a2f278772532e37de8
Sota Iguchi (e-chan1007) <echan041007@gmail.com>
2026-01-19 16:21:48 +0000

    chore: LP作成中

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,10 +1,19 @@
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 export default defineConfig({
   adapter: cloudflare(),
   output: "server",
+
   security: {
-    checkOrigin: false
-  }
+    checkOrigin: false,
+  },
+
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
